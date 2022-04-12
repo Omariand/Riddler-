8286f03379163bfb4b42611927adc269b61f9007
ProdBerge <michael.roberge17@gmail.com>
2022-04-12 16:04:42 -0400

    Update riddler.py

--- riddler.py
+++ riddler.py
@@ -1,13 +1,25 @@
 from wsgiref.util import shift_path_info
-
+from time import time
 
 class Time:
     
-    from time import time
 
 class Player:
 
     def __init__(self,player):
         self.player= player
     def __repr__(self):
-        command+shift_path_info
+    
+        #for guesses or like guess class
+    #make a function that takes the guesses
+    #a way to take off time from the counter when a bad guess is inputed
+    #2 minutes for a guess, 1st guess takes off 10 seconds, 2nd takes off 20 seconds
+    #3rd is 30 seconds. after 3rd guess every guess is 30 seconds
+    
+
+    #loop - would be inside the guessing and the time
+    #loop - for the riddle (repeating the riddle)
+    #if statement - if guess is correct, winner moves to next round
+    #next round function - play game, rules, winner function
+    #read txt file (1) - would read the file and take riddles
+    #read txt file (2) - would read the file and take anwsers
